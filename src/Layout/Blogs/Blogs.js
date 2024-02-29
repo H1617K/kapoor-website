0d0cadacd966f94e8c975b41f6a7ed8d1f2b346c
harsh <harsh@WHOHARSH>
2024-02-29 10:58:01 +0530

    set navigate to book Appoinment

--- src/Layout/Blogs/Blogs.js
+++ src/Layout/Blogs/Blogs.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../Blogs/Blogs.css'
 import { BlogImg1, BlogImg2, BlogImg3, BlogImg4, BlogImg5, BlogImg6 } from '../../assets/Image'
+import { NavLink } from 'react-router-dom'
 
 export const Blogs = () => {
   return (
     <div className="blog-container">
         {/* Blog1 */}
       <div className="blog-post">
+        <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg1} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Kanchipuram Saree Festive Wear</h2>
@@ -14,9 +16,11 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
         {/* Blog2 */}
       <div className="blog-post">
+      <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg2} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Meenakari Bandhani Woven</h2>
@@ -24,9 +28,11 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
         {/* Blog3 */}
       <div className="blog-post">
+      <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg3} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Handloom Saree Festive Wear</h2>
@@ -34,9 +40,11 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
         {/* Blog4 */}
       <div className="blog-post">
+      <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg4} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Handloom Saree Festive Wear</h2>
@@ -44,9 +52,11 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
         {/* Blog5 */}
       <div className="blog-post">
+      <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg5} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Handloom Saree Festive Wear</h2>
@@ -54,9 +64,11 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
       {/* Blog6 */}
       <div className="blog-post">
+      <NavLink to="/VideoAppointment">
       <img className='blog-image' src={BlogImg6} alt="Google Play" />
         <div className="blog-content">
           <h2 className="blog-title">Kanchipuram Saree Festive Wear</h2>
@@ -64,6 +76,7 @@ export const Blogs = () => {
           The Navy Blue Banarasi Silk Bandhani Woven Saree is a mesmerizing piece of traditional Indian craftsmanship and elegance. This exquisite saree...
           </p>
         </div>
+        </NavLink>
       </div>
     </div>
   )
